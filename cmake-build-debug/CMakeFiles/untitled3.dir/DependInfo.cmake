34560d46553334d88f2adb07c01e6660437a7391
taismith123 <42554501+taismith123@users.noreply.github.com>
2020-12-14 09:49:23 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/untitled3.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/taismith/CLionProjects/360/earl.cpp" "/Users/taismith/CLionProjects/360/cmake-build-debug/CMakeFiles/untitled3.dir/earl.cpp.o"
+  "/Users/taismith/CLionProjects/360/idk.cpp" "/Users/taismith/CLionProjects/360/cmake-build-debug/CMakeFiles/untitled3.dir/idk.cpp.o"
+  "/Users/taismith/CLionProjects/360/p.cpp" "/Users/taismith/CLionProjects/360/cmake-build-debug/CMakeFiles/untitled3.dir/p.cpp.o"
+  "/Users/taismith/CLionProjects/360/parser.cpp" "/Users/taismith/CLionProjects/360/cmake-build-debug/CMakeFiles/untitled3.dir/parser.cpp.o"
+  "/Users/taismith/CLionProjects/360/poop.cpp" "/Users/taismith/CLionProjects/360/cmake-build-debug/CMakeFiles/untitled3.dir/poop.cpp.o"
+  "/Users/taismith/CLionProjects/360/try.cpp" "/Users/taismith/CLionProjects/360/cmake-build-debug/CMakeFiles/untitled3.dir/try.cpp.o"
+  "/Users/taismith/CLionProjects/360/txtex.cpp" "/Users/taismith/CLionProjects/360/cmake-build-debug/CMakeFiles/untitled3.dir/txtex.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled3.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled3.dir/txtex.cpp.o"
+  "CMakeFiles/untitled3.dir/try.cpp.o"
+  "CMakeFiles/untitled3.dir/parser.cpp.o"
+  "CMakeFiles/untitled3.dir/p.cpp.o"
+  "CMakeFiles/untitled3.dir/idk.cpp.o"
+  "CMakeFiles/untitled3.dir/earl.cpp.o"
+  "CMakeFiles/untitled3.dir/poop.cpp.o"
+  "untitled3.pdb"
+  "untitled3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
